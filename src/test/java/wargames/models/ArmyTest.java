f57bf7abf775b409461a56ad0cdec83ca27497ee
kajtek <kajtek0123@gmail.com>
2025-07-15 18:38:46 +0200

    refactor: remove unused import

--- src/test/java/wargames/models/ArmyTest.java
+++ src/test/java/wargames/models/ArmyTest.java
@@ -3,7 +3,6 @@ package wargames.models;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
