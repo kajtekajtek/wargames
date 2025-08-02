714e82562e13eb03d65b0d7b04be1767f8cbaaca
kajtek <kajtek0123@gmail.com>
2025-08-02 15:54:53 +0200

    feat: add empty JSONStorage class

---
+++ src/main/java/wargames/storage/JSONStorage.java
@@ -0,0 +1,10 @@
+package wargames.storage;
+
+import wargames.models.*;
+
+public class JSONStorage implements StorageStrategy {
+
+    public void save(General general) {}
+    public void load(General general) {}
+
+}
