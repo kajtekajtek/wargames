3531ce234fbbd26750fc13b500c83a02d5aa0229
kajtek <kajtek0123@gmail.com>
2025-08-04 14:20:03 +0200

    feat: add setDirectory method to JSONStorage empty class

--- src/main/java/wargames/storage/JSONStorage.java
+++ src/main/java/wargames/storage/JSONStorage.java
@@ -4,6 +4,13 @@ import wargames.models.*;
 
 public class JSONStorage implements StorageStrategy {
 
+    public JSONStorage() {
+    }
+
+    public void setDirectory(String directoryPath) {
+
+    }
+
     public void save(General general) {}
     public void load(General general) {}
 
