1d6166322d17223f289e3f1afa6baacf30cd5397
kajtek <kajtek0123@gmail.com>
2025-08-07 11:58:31 +0200

    feat: basic JSONStorage save method functionality

--- src/main/java/wargames/storage/JSONStorage.java
+++ src/main/java/wargames/storage/JSONStorage.java
@@ -2,10 +2,17 @@ package wargames.storage;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import wargames.exceptions.StorageExceptions.JSONStorageExceptions.LoadJSONStorageException;
+import wargames.exceptions.StorageExceptions.JSONStorageExceptions.SaveJSONStorageException;
 import wargames.models.*;
 
 public class JSONStorage implements StorageStrategy {
@@ -13,22 +20,54 @@ public class JSONStorage implements StorageStrategy {
     private static final String DEFAULT_DIRECTORY_PATH = "data";
     private static final String FILE_EXTENSION = ".json";
 
-    private String directoryPath;
+    private final ObjectMapper mapper = Mapper.getInstance();
+
+    private String directoryPathString;
 
     public JSONStorage() {
-        this.directoryPath = DEFAULT_DIRECTORY_PATH;
+        this.directoryPathString = DEFAULT_DIRECTORY_PATH;
     }
 
-    public JSONStorage(String directoryPath) {
-        this.directoryPath = directoryPath;
+    public JSONStorage(String directoryPathString) {
+        this.directoryPathString = directoryPathString;
     }
 
-    public void save(General general) throws IllegalStateException {
+    public void save(General general) throws SaveJSONStorageException {
+        Path filePath = prepareFilePath(general);
+        createDirectories(filePath.getParent());
 
+        writeToFile(filePath, general);
+        
     }
 
-    public void load(General general) {
-
+    private Path prepareFilePath(General g) {
+        return Path.of(directoryPathString, g.getName() + FILE_EXTENSION);
     }
+
+    private void createDirectories(Path p) throws SaveJSONStorageException {
+        try {
+            Files.createDirectories(Path.of(directoryPathString));
+        } catch (IOException e) {
+            throw new SaveJSONStorageException(e.getMessage());
+        }
+    }
+
+    private void writeToFile(Path path, General general) throws SaveJSONStorageException {
+        try (FileChannel channel = FileChannel.open(path,
+                                                    StandardOpenOption.CREATE,
+                                                    StandardOpenOption.WRITE,
+                                                    StandardOpenOption.TRUNCATE_EXISTING);
+            FileLock lock = channel.lock();
+            BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+
+            this.mapper.writeValue(writer, general);
+
+        } catch (IOException e) {
+            throw new SaveJSONStorageException(e.getMessage());
+
+        }
+    }
+
+    public void load(General g) throws LoadJSONStorageException { }
 
 }
