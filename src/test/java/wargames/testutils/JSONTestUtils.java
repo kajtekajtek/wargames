85c578655c28155c4ae7721e09c041bc38f293c6
kajtek <kajtek0123@gmail.com>
2025-08-07 11:12:22 +0200

    feat: introduce more developed JSON nodes checks in testutils

--- src/test/java/wargames/testutils/JSONTestUtils.java
+++ src/test/java/wargames/testutils/JSONTestUtils.java
@@ -11,26 +11,80 @@ public final class JSONTestUtils {
     private JSONTestUtils() { }
 
     public static void assertJsonNodeContents(General g, JsonNode jn) {
-        assertNotNull(jn);
+        assertNotNull(jn, "General JSON node shouldn't be null");
 
-        assertNotNull(jn.get("name"));
-        String nameField = jn.get("name").asText();
+        assertNotNull(jn.get("name"), 
+            "General JSON field \"name\" is null");
+        String name = jn.get("name").asText();
+        assertEquals(g.getName(), name, 
+            "General JSON field \"name\" not equal to expected."
+        );
 
-        assertNotNull(jn.get("gold"));
-        int goldField = jn.get("gold").asInt();
-
-        assertEquals(g.getName(), nameField);
-        assertEquals(g.getGold(), goldField);
+        assertNotNull(jn.get("gold"),
+            "General JSON field \"gold\" is null");
+        int gold = jn.get("gold").asInt();
+        assertEquals(g.getGold(), gold,
+            "General JSON field \"gold\" not equal to expected." 
+        );
     }
 
     public static void assertJsonNodeContents(Army a, JsonNode jn) {
-        assertTrue(jn.isArray());
-        assertEquals(a.getSize(), jn.size());
+        assertNotNull(jn, "Army JSON node shouldn't be null");
+
+        assertNotNull(jn.get("soldiers"), 
+            "Army JSON field \"soldiers\" is null");
+        assertTrue(jn.get("soldiers").isArray());
+
+        assertNotNull(jn.get("empty"),
+            "Army JSON field \"empty\" is null");
+        Boolean empty = jn.get("empty").asBoolean();
+        assertEquals(a.isEmpty(), empty,
+            "Army JSON field \"empty\" not equal to expected."
+        );
+
+        assertNotNull(jn.get("size"),
+            "Army JSON field \"size\" is null");
+        int size = jn.get("size").asInt();
+        assertEquals(a.getSize(), size,
+            "Army JSON field \"size\" not equal to expected." 
+        );
+
+        assertNotNull(jn.get("totalStrength"),
+            "Army JSON field \"totalStrength\" is null");
+        int totalStrength = jn.get("totalStrength").asInt();
+        assertEquals(a.getTotalStrength(), totalStrength,
+            "Army JSON field \"totalStrength\" not equal to expected." 
+        );
     }
 
     public static void assertJsonNodeContents(Soldier s, JsonNode jn) {
-        assertEquals(s.getRank().name(), jn.get("rank").asText());
-        assertEquals(s.getExp(), jn.get("exp").asInt());
-        assertEquals(s.isAlive(), jn.get("alive").asBoolean());
+        assertNotNull(jn, "Soldier JSON node shouldn't be null");
+
+        assertNotNull(jn.get("rank"),
+            "Soldier JSON field \"rank\" is null");
+        String rank = jn.get("rank").asText();
+        assertEquals(s.getRank().toString(), rank,
+            "Soldier JSON field \"rank\" not equal to expected."
+        );
+
+        assertNotNull(jn.get("exp"),
+            "Soldier JSON field \"exp\" is  null");
+        int exp = jn.get("exp").asInt();
+        assertEquals(s.getExp(), exp,
+            "Soldier JSON field \"exp\" not equal to expected."
+        );
+        
+        assertNotNull(jn.get("alive"),
+            "Soldier JSON field \"alive\" is  null");
+        Boolean alive = jn.get("alive").asBoolean();
+        assertEquals(s.isAlive(), alive,
+            "Soldier JSON field \"alive\" not equal to expected."
+        );
+        
+        assertNotNull(jn.get("strength"),
+            "Soldier JSON field \"strength\" is  null");
+        int strength = jn.get("strength").asInt();
+        assertEquals(s.getStrength(), strength,
+            "Soldier JSON field \"strength\" not equal to expected.");
     }
 }
