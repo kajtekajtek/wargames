616ca3cacc37b149ae308cfeaab3267f6c130aa3
kajtek <kajtek0123@gmail.com>
2025-08-10 13:57:15 +0200

    refactor: use constructors instead of constructor wrappers for Soldier creation

--- src/main/java/wargames/models/Soldier.java
+++ src/main/java/wargames/models/Soldier.java
@@ -8,16 +8,19 @@ public class Soldier
 
     public static final int PROMOTION_RANK_MULTIPLIER = 5;
 
-    // constructors
-    public static Soldier withRank(Rank rank) {
-        return new Soldier(rank, 1, true);
+    public Soldier(Rank rank) {
+        this.rank  = rank;
+        this.exp   = 1;
+        this.alive = true;
+    }    
+
+    public Soldier(Rank rank, int exp) {
+        this.rank  = rank;
+        this.exp   = exp;
+        this.alive = true;
     }
 
-    public static Soldier withRankAndExp(Rank rank, int exp) {
-        return new Soldier(rank, exp, true);
-    }
-
-    private Soldier(Rank rank, int exp, boolean alive) {
+    public Soldier(Rank rank, int exp, boolean alive) {
         this.rank  = rank;
         this.exp   = exp;
         this.alive = alive;
