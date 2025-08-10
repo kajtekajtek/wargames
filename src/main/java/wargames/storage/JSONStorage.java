eeb82bfedbe55c0d5f935d9e808e460dcd942ee7
kajtek <kajtek0123@gmail.com>
2025-08-10 16:26:50 +0200

    feat: add additional Exception checks and openFileChannel helper method to JSONStorage

--- src/main/java/wargames/storage/JSONStorage.java
+++ src/main/java/wargames/storage/JSONStorage.java
@@ -1,19 +1,23 @@
 package wargames.storage;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+import java.nio.charset.StandardCharsets;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import wargames.exceptions.StorageExceptions.JSONStorageExceptions.LoadJSONStorageException;
-import wargames.exceptions.StorageExceptions.JSONStorageExceptions.SaveJSONStorageException;
+import wargames.exceptions.StorageExceptions.JSONStorageExceptions.*;
 import wargames.models.*;
 
 public class JSONStorage implements StorageStrategy {
@@ -38,7 +42,6 @@ public class JSONStorage implements StorageStrategy {
         createDirectories(filePath.getParent());
 
         writeValueToFile(filePath, general);
-        
     }
 
     private Path prepareFilePath(General g) {
@@ -48,46 +51,62 @@ public class JSONStorage implements StorageStrategy {
     private void createDirectories(Path p) throws SaveJSONStorageException {
         try {
             Files.createDirectories(Path.of(directoryPathString));
+
         } catch (IOException e) {
             throw new SaveJSONStorageException(
                 "could not create directory: " + getExceptionMessage(e)
             );
+
         }
     }
 
     private void writeValueToFile(Path path, General general) throws SaveJSONStorageException {
         try (
-            FileChannel channel = FileChannel.open(
-                path,
-                StandardOpenOption.CREATE,
-                StandardOpenOption.WRITE,
-                StandardOpenOption.TRUNCATE_EXISTING
-            );
-            FileLock       lock   = channel.lock();
-            BufferedWriter writer = Files.newBufferedWriter(
+            FileChannel    channel = openFileChannel(path);
+            FileLock       lock    = channel.lock();
+            BufferedWriter writer  = Files.newBufferedWriter(
                 path, StandardCharsets.UTF_8
             );
+
         ) {
             this.mapper.writeValue(writer, general);
+
+        } catch (StreamReadException e) {
+            throw new SaveJSONStorageException(
+                "unable to parse object to be saved at " + path + ": " 
+                + getExceptionMessage(e)
+            );
+    
+        } catch (DatabindException e) {
+            throw new SaveJSONStorageException(
+                "unable to bind data to save at " + path + ": " 
+                + getExceptionMessage(e)
+            );
 
         } catch (IOException e) {
             throw new SaveJSONStorageException(
                 "unable to write " + path + ": " + getExceptionMessage(e)
             );
+
         }
     }
 
-    public void load(General general) throws LoadJSONStorageException {
-        Path filePath = prepareFilePath(general);
-        try {
-            General loaded = readValueFromFile(filePath, general);
-            general.setArmy(loaded.getArmy());
-            general.setName(loaded.getName());
-            general.setGold(loaded.getGold());
+    private FileChannel openFileChannel(Path path) throws IOException {
+        return FileChannel.open(
+            path,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.WRITE,
+            StandardOpenOption.TRUNCATE_EXISTING
+        );
+    }
 
-        } catch (Exception e) {
-            throw new LoadJSONStorageException(getExceptionMessage(e));
-        }
+    public void load(General general) throws LoadJSONStorageException {
+        Path    filePath = prepareFilePath(general);
+        General loaded   = readValueFromFile(filePath, general);
+
+        general.setArmy(loaded.getArmy());
+        general.setName(loaded.getName());
+        general.setGold(loaded.getGold());
     }
 
     private General readValueFromFile(Path path, General general) throws LoadJSONStorageException {
@@ -98,9 +117,22 @@ public class JSONStorage implements StorageStrategy {
         ) {
             return this.mapper.readValue(reader, General.class);
 
+        } catch (StreamReadException e) {
+            throw new LoadJSONStorageException(
+                "unable to parse " + path + ": " 
+                + getExceptionMessage(e)
+            );
+        
+        } catch (DatabindException e) {
+            throw new LoadJSONStorageException(
+                "unable to bind data from " + path + ": " 
+                + getExceptionMessage(e)
+            );
+
         } catch (IOException e) {
             throw new LoadJSONStorageException(
-                "unable to read " + path + ": " + getExceptionMessage(e)
+                "unable to read " + path + ": " + 
+                getExceptionMessage(e)
             );
         }
     }
