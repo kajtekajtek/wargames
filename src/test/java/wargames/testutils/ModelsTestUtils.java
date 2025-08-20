9fa654c40600d23448d6e3d5d666252671bef4b7
kajtek <kajtek0123@gmail.com>
2025-08-20 17:27:38 +0200

    refactor: move getJSONFileFromDirectory to JSONTestUtils

--- src/test/java/wargames/testutils/ModelsTestUtils.java
+++ src/test/java/wargames/testutils/ModelsTestUtils.java
@@ -8,28 +8,34 @@ public final class ModelsTestUtils {
 
     private ModelsTestUtils() { }
 
-    public static void assertEqualGenerals(General expected, General actual) {
-        assertEquals(expected.getName(), actual.getName());
-        assertEquals(expected.getGold(), actual.getGold());
+    public static void assertEquals(General expected, General actual) {
+        org.junit.jupiter.api.Assertions
+            .assertEquals(expected.getName(), actual.getName());
+        org.junit.jupiter.api.Assertions
+            .assertEquals(expected.getGold(), actual.getGold());
 
         Army expectedArmy = expected.getArmy();
         Army actualArmy   = actual.getArmy(); 
-        assertEqualArmies(expectedArmy, actualArmy); 
+        assertEquals(expectedArmy, actualArmy); 
     }
 
-    public static void assertEqualArmies(Army expected, Army actual) {
-        assertEquals(expected.getSize(), actual.getSize());
+    public static void assertEquals(Army expected, Army actual) {
+        org.junit.jupiter.api.Assertions
+            .assertEquals(expected.getSize(), actual.getSize());
 
         for (int i = 0; i < expected.getSize(); i++) {
             Soldier expectedSoldier = expected.getSoldiers().get(i);
             Soldier actualSoldier   = actual.getSoldiers().get(i);
-            assertEqualSoldiers(expectedSoldier, actualSoldier);
+            assertEquals(expectedSoldier, actualSoldier);
         }
     }
 
-    public static void assertEqualSoldiers(Soldier expected, Soldier actual) {
-        assertEquals(expected.getExp(),  actual.getExp());
-        assertEquals(expected.getRank(), actual.getRank());
-        assertEquals(expected.isAlive(), actual.isAlive());
+    public static void assertEquals(Soldier expected, Soldier actual) {
+        org.junit.jupiter.api.Assertions
+            .assertEquals(expected.getExp(),  actual.getExp());
+        org.junit.jupiter.api.Assertions
+            .assertEquals(expected.getRank(), actual.getRank());
+        org.junit.jupiter.api.Assertions
+            .assertEquals(expected.isAlive(), actual.isAlive());
     }
 }
